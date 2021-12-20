fb77407c2cfc596a9f8725b79b18829243ce77ad
Deepak yadav <deepakyadav@deepaks-mini.attlocal.net>
2021-12-19 22:28:44 -0800

    remove unnecessary line

--- src/main/java/TestDemo2.java
+++ src/main/java/TestDemo2.java
@@ -1,7 +1,6 @@
 public class TestDemo2 {
 
     public static void main (String args[]){
-        System.out.println("testgemo2");
-        System.out.println("testgemo2");
+        System.out.println("test");
     }
 }
